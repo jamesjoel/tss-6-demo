ce093f70cf385d1f6870ffde9e20e96029b9889b
jamesjoel <james.joelj@gmail.com>
2025-11-12 17:16:07 +0530

    my personal changes

--- myproject/app.js
+++ myproject/app.js
@@ -16,9 +16,31 @@ app.get("/api/v1/student", (req, res)=>{
     
     res.send(stu)
 })
+app.get("/api/v1/student", (req, res)=>{
+    
+    res.send(stu)
+})
+app.get("/api/v1/student", (req, res)=>{
+    
+    res.send(stu)
+})
+app.get("/api/v1/student", (req, res)=>{
+    
+    res.send(stu)
+})
+app.get("/api/v1/student", (req, res)=>{
+    
+    res.send(stu)
+})
+app.get("/api/v1/student", (req, res)=>{
+    
+    res.send(stu)
+})
 
 
 const PORT = 3000;
+
 app.listen(PORT, ()=>{
     console.log("Server Running With Port", PORT)
 })
+
